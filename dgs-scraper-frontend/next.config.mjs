9a83bbecf01922ec881db66561e5b8cbf6e5cadc
Benjamin Schoolland <bschoolland@gmail.com>
2025-08-12 19:00:26 -0700

    feat: add base path

--- dgs-scraper-frontend/next.config.mjs
+++ dgs-scraper-frontend/next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const BASE_PATH = process.env.BASE_PATH;
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -9,6 +11,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  ...(BASE_PATH ? { basePath: BASE_PATH.startsWith('/') ? BASE_PATH : `/${BASE_PATH}` } : {}),
 }
 
 export default nextConfig
