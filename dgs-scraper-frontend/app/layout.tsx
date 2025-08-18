0319a178767e39db247cdd4e6b736a7385ee6fee
Benjamin Schoolland <bschoolland@gmail.com>
2025-08-18 13:35:30 -0700

    feat: rename site

--- dgs-scraper-frontend/app/layout.tsx
+++ dgs-scraper-frontend/app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'DSA Scraper',
+  description: 'Scrapes leads from the DSA website and sends them to your email',
+  generator: '',
 }
 
 export default function RootLayout({
